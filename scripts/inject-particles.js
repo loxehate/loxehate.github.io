301a8ae13094e36e5cef838537007fdad4d4df6e
nanjy <9317869+nan1234@user.noreply.gitee.com>
2025-06-13 16:01:16 +0800

    update

--- scripts/inject-particles.js
+++ scripts/inject-particles.js
@@ -1,7 +1,7 @@
 hexo.extend.filter.register('theme_inject', function (injects) {
   // 注入容器到 body 的顶部
   injects.bodyBegin.raw('particles-div', `
-    <div id="particles-js" style="position: fixed; z-index: -1; width: 100%; height: 100%; top: 0; left: 0;"></div>
+    <div id="particles-js" style="position: fixed; z-index: 0; width: 100%; height: 100%; top: 0; left: 0;"></div>
   `);
 
   // 注入脚本到页面底部 footer
