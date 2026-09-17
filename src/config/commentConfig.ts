7f601012016d163f815365f1691df4eec5860230
nanjy <9317869+nan1234@user.noreply.gitee.com>
2026-09-17 17:46:51 +0800

    update

--- src/config/commentConfig.ts
+++ src/config/commentConfig.ts
@@ -54,7 +54,7 @@ export const commentConfig: CommentConfig = {
 	//giscus评论系统配置
 	giscus: {
 		// 设置 Giscus 评论系统仓库
-		repo: "https://loxehate.github.io/",
+		repo: "loxehate/loxehate.github.io",
 		// 设置 Giscus 评论系统仓库ID
 		repoId: "R_kgDOOmV_sg",
 		// 设置 Giscus 评论系统分类
